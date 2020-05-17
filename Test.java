7b42eb1caa41727d9d36a0510f8d775b9fbd06ba
Pydare <dareyadewumi650@gmail.com>
2020-05-17 21:35:39 +0100

    progress

--- Test.java
+++ Test.java
@@ -1,13 +1,18 @@
+/*
+**
+ * author :   	Dare Adewumi
+ * created : 	Thu May 14 19:26:30 2020
+**
+*/
 
 public class Test {
-    final int value = 10;
-
-    //Examples of declaring constants in java
-    public static final int BOXWIDTH = 6;
-    static final String TITLE = "Manager";
     
-    public void changeValue(){
+    public static void main(String[] args){
         
-    }
+        String s = new String("Welcome-to-Tutorialspoint.com");
+
+        System.out.println("Return value: ");
+        System.out.println(s.split("-", 2));
+    }   
 
 }
